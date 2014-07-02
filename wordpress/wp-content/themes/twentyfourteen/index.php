669ceee8b5cd0c3416dea5795ce387515534c9e0
parpelli@yahoo.com <riasia123>
2014-07-02 15:23:45 -0500

    Testing deployment

--- wordpress/wp-content/themes/twentyfourteen/index.php
+++ wordpress/wp-content/themes/twentyfourteen/index.php
@@ -38,6 +38,7 @@ get_header(); ?>
 					 * use this in a child theme, then include a file called called content-___.php
 					 * (where ___ is the post format) and that will be used instead.
 					 */
+                    echo ("Just a test");
 					get_template_part( 'content', get_post_format() );
 
 				endwhile;
