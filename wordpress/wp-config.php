911ca0aa42d7e03a8855fa991975f34a65307158
parpelli@yahoo.com <riasia123>
2014-07-07 15:45:59 -0500

    Added Multisite capability.

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -33,6 +33,9 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+/* Multisite */
+define( 'WP_ALLOW_MULTISITE', true );
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
